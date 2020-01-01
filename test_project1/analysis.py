f30d751484d3143c9e0544207cc903004181a68d
Elliot Tennison <elliot.s.tennison@gmail.com>
2020-01-01 23:07:52 +0800

    completed pie chart creator

--- test_project1/analysis.py
+++ test_project1/analysis.py
@@ -2,68 +2,68 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-df = pd.read_csv (r'suicide-rates-overview-1985-to-2016/master.csv')
+complete_data_set = pd.read_csv (r'suicide-rates-overview-1985-to-2016/master.csv')
 
-no_of_males = 0
-no_of_females = 0
+
 countries = []
-
-#calculates male/female proportion and creates stores countries
-for row in df.itertuples():
+for row in complete_data_set.itertuples():
   if not (row.country in countries):
     countries.append(row.country)
 
-  if row.sex == 'male':
-    no_of_males += 1
-  else:
-    no_of_females += 1
+def gender_proportion():
+  no_of_males = 0
+  no_of_females = 0
 
-total = no_of_males + no_of_females
-percentage_of_males = (no_of_males / total)*100
-percentage_of_females = (no_of_females / total)*100
+  #calculates male/female proportion and creates stores countries
+  for row in complete_data_set.itertuples():
+    if row.sex == 'male':
+      no_of_males += 1
+    else:
+      no_of_females += 1
+
+  total = no_of_males + no_of_females
+  percentage_of_males = (no_of_males / total)*100
+  percentage_of_females = (no_of_females / total)*100
+
+  print("males: " + str(percentage_of_males) + '%', "Number of females: " + str(percentage_of_females) + '%')
 
 
-print("males: " + str(percentage_of_males) + '%', "Number of females: " + str(percentage_of_females) + '%')
 
-################################################
-#ALBANIAN EXAMPLE
+def highest_suicide_rates():
+  suicides_no = []
+  suicide_totals = pd.DataFrame(columns=['country', 'total'])
 
-#filters only albanian suicides into var variable
-albanian_data = df[df.country == 'Albania']
+  year = input("Input a year: ")
+  num = input("How many top contries: ")
 
-x = albanian_data.year
-y = albanian_data.suicides_no
-#plt.plot(x,y)
-#plt.show() this line stops code execution so comment while experimenting
+  i = 0
 
-################################################
-#filter data by year, see breakdown of total number of suicides over countries in specified year
+  for country in countries:
+    data_for_given_year = complete_data_set[complete_data_set.year == int(year)]
+    #filter by year input by user
 
-suicides_no = []
-data_store = {}
-year = input("Input a year: ")
+    filtered_data = data_for_given_year[data_for_given_year.country == country]
+    #filter by each country in the array
 
-for country in countries:
-  data1 = df[df.year == int(year)]
-  #filter by year input by user
-  data = data1[data1.country == country]
-  #filter by each country in the array
+    amount = 0 # accumulator for suicides in year and country
 
-  amount = 0
-  for row in data.itertuples():
-    amount += row.suicides_no
+    for row in filtered_data.itertuples(): #sums amount of suicides in one country
+      amount += row.suicides_no
 
-  suicides_no.append(amount)
-  data_store[country] = amount
+    suicide_totals.loc[i] = [country, amount] #should add new row to suicide_totals
+    i += 1
 
-ordered_data = sorted(data_store)
+  ordered_data = suicide_totals.sort_values('total', ascending=False).head(int(num))
 
-print(ordered_data)
-fig = plt.figure()
-ax = fig.add_axes([0,0,1,1])
-ax.set_xlabel('Countries')
-ax.set_ylabel('Number')
-ax.set_title('Scores by group and gender')
-ax.pie(suicides_no,labels = countries)
-plt.show()
+  fig = plt.figure()
+  ax = fig.add_axes([0,0,1,1])
+  ax.pie(ordered_data.total, labels = ordered_data.country)
+  plt.show()
 
+
+highest_suicide_rates()
+
+
+
+def generation_proportion():
+  print("g")
